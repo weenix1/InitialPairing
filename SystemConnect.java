3603663cb27ed2231ee0965e88cfcc7bb6607850
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-12-14 21:52:16 +0100

    Initial commit

---
+++ SystemConnect.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class SystemConnect here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SystemConnect
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SystemConnect
+     */
+    public SystemConnect()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
